aab7d31b1c7c3cbd1b0fec3e42c1914d310e2654
SoulEmper0r <goroshekvbanochke@gmail.com>
2024-10-01 22:10:03 +0300

    Файлы сайта-визитки

---
+++ Site/js/MyJs.js
@@ -0,0 +1,230 @@
+$(document).ready(function() {
+    document.body.style.cursor = 'url("./images/cursor.png"), auto';
+    // Открываем первое модальное окно
+    $("#triggerModal1").on("click", function() {
+        $("#myModal1").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем первое модальное окно
+        $("#closeModal1").on("click", function() {
+            $("#myModal1").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем второе модальное окно
+    $("#triggerModal2").on("click", function() {
+        $("#myModal2").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем второе модальное окно
+        $("#closeModal2").on("click", function() {
+            $("#myModal2").fadeOut(); // Используем jQuery для анимации
+        });
+    
+    // Открываем третье модальное окно
+    $("#triggerModal3").on("click", function() {
+        $("#myModal3").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем третье модальное окно
+        $("#closeModal3").on("click", function() {
+            $("#myModal3").fadeOut(); // Используем jQuery для анимации
+        });
+    
+    // Открываем четвертое модальное окно
+    $("#triggerModal4").on("click", function() {
+        $("#myModal4").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем четвертое модальное окно
+        $("#closeModal4").on("click", function() {
+            $("#myModal4").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем пятое модальное окно
+    $("#triggerModal5").on("click", function() {
+        $("#myModal5").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем пятое модальное окно
+        $("#closeModal5").on("click", function() {
+            $("#myModal5").fadeOut(); // Используем jQuery для анимации
+        })
+
+    // Открываем шестое модальное окно
+    $("#triggerModal6").on("click", function() {
+        $("#myModal6").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем шестое модальное окно
+        $("#closeModal6").on("click", function() {
+            $("#myModal6").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем седьмое модальное окно
+    $("#triggerModal7").on("click", function() {
+        $("#myModal7").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем седьмое модальное окно
+        $("#closeModal7").on("click", function() {
+            $("#myModal7").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем восьмое модальное окно
+    $("#triggerModal8").on("click", function() {
+        $("#myModal8").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем восьмое модальное окно
+        $("#closeModal8").on("click", function() {
+            $("#myModal8").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем девятое модальное окно
+    $("#triggerModal9").on("click", function() {
+        $("#myModal9").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем девятое модальное окно
+        $("#closeModal9").on("click", function() {
+            $("#myModal9").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем десятое модальное окно
+    $("#triggerModal10").on("click", function() {
+        $("#myModal10").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем десятое модальное окно
+        $("#closeModal10").on("click", function() {
+            $("#myModal10").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем одиннадцатое модальное окно
+    $("#triggerModal11").on("click", function() {
+        $("#myModal11").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем одиннадцатое модальное окно
+        $("#closeModal11").on("click", function() {
+            $("#myModal11").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем двенадцатое модальное окно
+    $("#triggerModal12").on("click", function() {
+        $("#myModal12").fadeIn(); // Используем jQuery для анимации
+    });
+
+        // Закрываем двенадцатое модальное окно
+        $("#closeModal12").on("click", function() {
+            $("#myModal12").fadeOut(); // Используем jQuery для анимации
+        });
+
+    // Открываем тринадцатое модальное окно
+    $("#triggerModal13").on("click", function() {
+        $("#myModal13").fadeIn();
+    });
+
+        // Закрываем тринадцатое модальное окно
+        $("#closeModal13").on("click", function() {
+            $("#myModal13").fadeOut();
+        });
+
+    // Открываем четырнадцатое модальное окно
+    $("#triggerModal14").on("click", function() {
+        $("#myModal14").fadeIn();
+    });
+
+        // Закрываем четырнадцатое модальное окно
+        $("#closeModal14").on("click", function() {
+            $("#myModal14").fadeOut();
+        });
+
+    // Открываем пятнадцатое модальное окно
+    $("#triggerModal15").on("click", function() {
+        $("#myModal15").fadeIn();
+    });
+
+        // Закрываем пятнадцатое модальное окно
+        $("#closeModal15").on("click", function() {
+            $("#myModal15").fadeOut();
+        });
+
+    // Открываем шестнадцатое модальное окно
+    $("#triggerModal16").on("click", function() {
+        $("#myModal16").fadeIn();
+    });
+
+        // Закрываем шестнадцатое модальное окно
+        $("#closeModal16").on("click", function() {
+            $("#myModal16").fadeOut();
+        });
+
+    // Открываем семнадцатое модальное окно
+    $("#triggerModal17").on("click", function() {
+        $("#myModal17").fadeIn();
+    });
+
+        // Закрываем семнадцатое модальное окно
+        $("#closeModal17").on("click", function() {
+            $("#myModal17").fadeOut();
+        });
+
+    // Открываем восемнадцатое модальное окно
+    $("#triggerModal18").on("click", function() {
+        $("#myModal18").fadeIn();
+    });
+
+        // Закрываем восемнадцатое модальное окно
+        $("#closeModal18").on("click", function() {
+            $("#myModal18").fadeOut();
+        });
+
+    // Открываем девятнадцатое модальное окно
+    $("#triggerModal19").on("click", function() {
+        $("#myModal19").fadeIn();
+    });
+
+        // Закрываем девятнадцатое модальное окно
+        $("#closeModal19").on("click", function() {
+            $("#myModal19").fadeOut();
+        });
+
+    // Открываем двадцатое модальное окно
+    $("#triggerModal20").on("click", function() {
+        $("#myModal20").fadeIn();
+    });
+
+        // Закрываем двадцатое модальное окно
+        $("#closeModal20").on("click", function() {
+            $("#myModal20").fadeOut();
+        });
+
+    // Открываем двадцать первое модальное окно
+    $("#triggerModal21").on("click", function() {
+        $("#myModal21").fadeIn();
+    });
+
+        // Закрываем двадцать первое модальное окно
+        $("#closeModal21").on("click", function() {
+            $("#myModal21").fadeOut();
+        });
+
+            // Открываем двадцать второе модальное окно
+    $("#triggerModal22").on("click", function() {
+        $("#myModal22").fadeIn();
+    });
+
+        // Закрываем двадцать второе модальное окно
+        $("#closeModal22").on("click", function() {
+            $("#myModal22").fadeOut();
+        });
+
+
+    // Закрываем модальное окно при клике вне него
+    $(window).on("click", function(event) {
+        if ($(event.target).is(".modal")) {
+            $(".modal").fadeOut(); // Закрываем все модальные окна
+        }
+    });
+});
---
+++ Site/js/typed.init.js
@@ -0,0 +1,25 @@
+
+// Typed Text
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 200,
+    typeSpeed: 200,           // Скорость появления букв
+    backSpeed: 200,           // Скорость удаления букв
+    backDelay: 1000,
+    loop: true,
+    strings: [
+        'Разработчик веб приложений', 'QA engineer',
+    ],
+});
+
+// var typed = new Typed('#typed2', {
+//     stringsElement: '#typed-strings',
+//     typeSpeed: 200,
+//     typeSpeed: 200,           // Скорость появления букв
+//     backSpeed: 200,           // Скорость удаления букв
+//     backDelay: 1000,
+//     loop: true,
+//     strings: [
+//         'Александр'
+//     ],
+// });
